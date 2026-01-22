2dbfe83722aa815d35187e9d48988b2dfd80e574
botoddly <250804054+botoddly@users.noreply.github.com>
2026-01-22 09:51:47 +0100

    Replace var with explicit types

--- tests/GameKit.Content.Tests/DictFileSystemFactory.cs
+++ tests/GameKit.Content.Tests/DictFileSystemFactory.cs
@@ -7,14 +7,14 @@ public static class DictFileSystemFactory
 {
     public static DictFileSystem Create()
     {
-        var files = new Dictionary<string, ImmutableArray<VirtualFile>>
+        FrozenDictionary<string, ImmutableArray<VirtualFile>> files = new Dictionary<string, ImmutableArray<VirtualFile>>
         {
             ["."] = [new ByteVirtualFile("a.txt", "Hello a"u8), new ByteVirtualFile("b.txt", "Hello b"u8)],
             ["dir1"] = [new ByteVirtualFile("dir1/dir1a.txt", "Hello dir1a"u8), new ByteVirtualFile("dir1/dir1b.txt", "Hello dir1b"u8)],
             ["dir2"] = [new ByteVirtualFile("dir2/dir2a.txt", "Hello dir2a"u8), new ByteVirtualFile("dir2/dir2b.txt", "Hello dir2b"u8)],
         }.ToFrozenDictionary();
 
-        var directories = new Dictionary<string, ImmutableArray<string>>
+        FrozenDictionary<string, ImmutableArray<string>> directories = new Dictionary<string, ImmutableArray<string>>
         {
             ["."] = ["dir1", "dir2"],
             ["dir1"] = [],
