f612d6994c0b368d47f58f94e221a97db00bb871
botoddly <250804054+botoddly@users.noreply.github.com>
2026-02-13 07:56:17 +0100

    Validate texture format support before creating GPU textures (#85)

---
+++ src/GameKit/Gpu/TextureType.cs
@@ -0,0 +1,12 @@
+using SDL;
+
+namespace GameKit.Gpu;
+
+public enum TextureType
+{
+    TwoD = SDL_GPUTextureType.SDL_GPU_TEXTURETYPE_2D,
+    TwoDArray = SDL_GPUTextureType.SDL_GPU_TEXTURETYPE_2D_ARRAY,
+    ThreeD = SDL_GPUTextureType.SDL_GPU_TEXTURETYPE_3D,
+    Cube = SDL_GPUTextureType.SDL_GPU_TEXTURETYPE_CUBE,
+    CubeArray = SDL_GPUTextureType.SDL_GPU_TEXTURETYPE_CUBE_ARRAY
+}
