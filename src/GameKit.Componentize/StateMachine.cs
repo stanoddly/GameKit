75d34d6ce7fa48954a97f6686e78e3173968d226
botoddly <250804054+botoddly@users.noreply.github.com>
2026-03-03 14:21:01 +0100

    Add StateMachine<TSelf, TState> component with command handling (#115)

---
+++ src/GameKit.Componentize/StateMachine.cs
@@ -0,0 +1,52 @@
+using System.Runtime.CompilerServices;
+
+namespace GameKit.Componentize;
+
+public abstract class StateMachine<TSelf, TState> : GameComponent
+    where TSelf : StateMachine<TSelf, TState>
+    where TState : StateMachine<TSelf, TState>.State
+{
+    private TState _state;
+
+    protected StateMachine(TState initialState) => _state = initialState;
+
+    protected internal override void OnAttach() => _state.Enter(Unsafe.As<TSelf>(this));
+    protected internal override void OnDetach() => _state.Exit(Unsafe.As<TSelf>(this));
+
+    public void Handle<TCommand>(in TCommand command) where TCommand : struct
+    {
+        if (_state is ICommandHandler<TCommand> handler)
+        {
+            handler.Handle(Unsafe.As<TSelf>(this), in command);
+        }
+    }
+
+    protected TState ChangeState(TState newState)
+    {
+        ArgumentNullException.ThrowIfNull(newState);
+        TSelf self = Unsafe.As<TSelf>(this);
+        TState oldState = _state;
+        _state = newState;
+        oldState.Exit(self);
+        // Exit triggered a reentrant transition; that transition wins.
+        if (_state != newState)
+        {
+            return _state;
+        }
+        newState.Enter(self);
+        return newState;
+    }
+
+    protected TState CurrentState => _state;
+
+    public abstract class State
+    {
+        public virtual void Enter(TSelf context) { }
+        public virtual void Exit(TSelf context) { }
+    }
+
+    public interface ICommandHandler<TCommand> where TCommand : struct
+    {
+        void Handle(TSelf context, in TCommand command);
+    }
+}
