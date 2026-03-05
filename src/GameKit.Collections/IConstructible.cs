4161f331d4ece97f14a576ea12559a812892ea63
botoddly <250804054+botoddly@users.noreply.github.com>
2026-03-05 08:47:29 +0100

    Keep generated .cs files in source control

---
+++ src/GameKit.Collections/IConstructible.cs
@@ -0,0 +1,42 @@
+namespace GameKit.Collections;
+
+public interface IConstructible<TSelf, TArg1>
+{
+    static abstract TSelf Construct(in TArg1 arg1);
+}
+public interface IConstructible<TSelf, TArg1, TArg2>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3, TArg4>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3, in TArg4 arg4);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3, TArg4, TArg5>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3, in TArg4 arg4, in TArg5 arg5);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3, in TArg4 arg4, in TArg5 arg5, in TArg6 arg6);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3, in TArg4 arg4, in TArg5 arg5, in TArg6 arg6, in TArg7 arg7);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3, in TArg4 arg4, in TArg5 arg5, in TArg6 arg6, in TArg7 arg7, in TArg8 arg8);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8, TArg9>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3, in TArg4 arg4, in TArg5 arg5, in TArg6 arg6, in TArg7 arg7, in TArg8 arg8, in TArg9 arg9);
+}
+public interface IConstructible<TSelf, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8, TArg9, TArg10>
+{
+    static abstract TSelf Construct(in TArg1 arg1, in TArg2 arg2, in TArg3 arg3, in TArg4 arg4, in TArg5 arg5, in TArg6 arg6, in TArg7 arg7, in TArg8 arg8, in TArg9 arg9, in TArg10 arg10);
+}
