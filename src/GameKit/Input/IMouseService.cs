d03b348d1f0f30168fd9258804583a70e0ba2671
botoddly <250804054+botoddly@users.noreply.github.com>
2026-05-06 19:30:00 +0200

    Expose mouse wheel input

--- src/GameKit/Input/IMouseService.cs
+++ src/GameKit/Input/IMouseService.cs
@@ -5,8 +5,10 @@ public interface IMouseService
     event MouseButtonPressedHandler ButtonPress;
     event MouseButtonReleasedHandler ButtonRelease;
     event MouseMotionHandler Motion;
+    event MouseWheelHandler Wheel;
 
     void SubscribeButtonPress(int priority, MouseButtonPressedHandler handler);
     void SubscribeButtonRelease(int priority, MouseButtonReleasedHandler handler);
     void SubscribeMotion(int priority, MouseMotionHandler handler);
+    void SubscribeWheel(int priority, MouseWheelHandler handler);
 }
