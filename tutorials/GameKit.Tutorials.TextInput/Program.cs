8896114f4017d941eb6a2cce98cf228afd85259b
botoddly <250804054+botoddly@users.noreply.github.com>
2026-05-08 07:18:43 +0200

    Add text input tutorial with name and dimension fields

---
+++ tutorials/GameKit.Tutorials.TextInput/Program.cs
@@ -0,0 +1,23 @@
+using GameKit.App;
+using GameKit.Pencuil;
+using GameKit.RenderOrchestration;
+
+namespace GameKit.Tutorials.TextInput;
+
+static class Program
+{
+    static int Main(string[] args)
+    {
+        GameKitAppBuilder builder = new GameKitAppBuilder()
+            .UseDefaultRenderManager()
+            .UsePencuil()
+            .AddContentFromProjectDirectory("../GameKit.Tutorials.Hotbar/Content");
+
+        builder.AddSingleton(new AppConfig { Size = (640, 400), Title = "Text Input" });
+        builder.AddSingleton(new TextInputViewModel());
+        builder.AddSingleton<IView, TextInputView>();
+
+        using IGameKitApp gameKitApp = builder.Build();
+        return gameKitApp.Run();
+    }
+}
---
+++ tutorials/GameKit.Tutorials.TextInput/TextInputView.cs
@@ -0,0 +1,113 @@
+using GameKit.Common;
+using GameKit.Gpu;
+using GameKit.Pencuil;
+using GameKit.Text;
+
+namespace GameKit.Tutorials.TextInput;
+
+public class TextInputViewModel : IViewModel
+{
+    public bool IsDirty { get; set; } = true;
+
+    private string _name = "Player";
+    private string _width = "64";
+    private string _height = "48";
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (_name != value)
+            {
+                _name = value;
+                IsDirty = true;
+            }
+        }
+    }
+
+    public string Width
+    {
+        get => _width;
+        set
+        {
+            if (_width != value)
+            {
+                _width = value;
+                IsDirty = true;
+            }
+        }
+    }
+
+    public string Height
+    {
+        get => _height;
+        set
+        {
+            if (_height != value)
+            {
+                _height = value;
+                IsDirty = true;
+            }
+        }
+    }
+}
+
+public class TextInputView : View<TextInputViewModel>
+{
+    private static readonly Color BackgroundColor = new(28, 30, 34, 255);
+    private static readonly Color LabelColor = new(180, 180, 180, 255);
+    private static readonly Color ValueColor = new(235, 238, 242, 255);
+
+    private readonly Font _font;
+    private readonly Font _labelFont;
+
+    public TextInputView(TextInputViewModel viewModel, IFontSystem fontSystem)
+        : base(viewModel)
+    {
+        _font = fontSystem.Load("fonts/GohuFont-Medium.ttf", 16);
+        _labelFont = fontSystem.Load("fonts/GohuFont-Medium.ttf", 14);
+    }
+
+    public override void Build(Pencil pencil)
+    {
+        pencil.MoveTo(0, 0);
+        pencil.Panel(pencil.BottomRight.X, pencil.BottomRight.Y, BackgroundColor);
+
+        int startX = pencil.Center.X - 120;
+        int startY = 80;
+
+        using (pencil.WithGap(12))
+        using (pencil.WithDirection(LayoutDirection.Bottom))
+        {
+            pencil.MoveTo(startX, startY);
+
+            pencil.Text("Name", _labelFont, LabelColor);
+
+            string name = ViewModel.Name;
+            if (pencil.TextField("name", ref name, _font, 240))
+            {
+                ViewModel.Name = name;
+            }
+
+            pencil.Text("Width", _labelFont, LabelColor);
+
+            string width = ViewModel.Width;
+            if (pencil.TextField("width", ref width, _font, 240))
+            {
+                ViewModel.Width = width;
+            }
+
+            pencil.Text("Height", _labelFont, LabelColor);
+
+            string height = ViewModel.Height;
+            if (pencil.TextField("height", ref height, _font, 240))
+            {
+                ViewModel.Height = height;
+            }
+        }
+
+        pencil.MoveTo(startX, startY + 280);
+        pencil.Text($"Name: {ViewModel.Name}  Size: {ViewModel.Width}x{ViewModel.Height}", _font, ValueColor);
+    }
+}
