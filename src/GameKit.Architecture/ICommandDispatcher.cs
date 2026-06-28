fa303094216b23f9adeb4b50b55064ba75902a50
botoddly <250804054+botoddly@users.noreply.github.com>
2026-06-28 22:32:53 +0200

    Define command acceptance semantics

--- src/GameKit.Architecture/ICommandDispatcher.cs
+++ src/GameKit.Architecture/ICommandDispatcher.cs
@@ -1,6 +1,10 @@
 namespace GameKit.Architecture;
 
+/// <summary>
+/// Dispatches commands and returns their handlers' acceptance results.
+/// </summary>
 public interface ICommandDispatcher
 {
+    /// <inheritdoc cref="ICommandHandler{TCommand}.Handle(TCommand)"/>
     bool Dispatch<TCommand>(TCommand command);
 }
